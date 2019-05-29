1ffb597cc213a9597e4ac57fca1912349928f9d7
nickyrayray <Enterprise.1701>
2019-05-29 00:08:32 -0400

    fix settings

--- settings.py
+++ settings.py
@@ -1,16 +1,24 @@
+import dj_database_url
 import os
 
 INSTALLED_APPS = ('game_data',)
 
 SECRET_KEY = '12345'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pubg_stats',
-        'USER': 'pubgadmin',
-        'PASSWORD': os.environ.get('PUBG_DB_PASS'),
-        'HOST': os.environ.get('PUBG_DB_HOST', 'localhost'),
-        'PORT': 5432
+databse_url = os.environ.get('DATABASE_URL', None)
+
+if databse_url:
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'pubg_stats',
+            'USER': 'pubgadmin',
+            'PASSWORD': os.environ.get('PUBG_DB_PASS'),
+            'HOST': os.environ.get('PUBG_DB_HOST', 'localhost'),
+            'PORT': 5432
+        }
+    }
