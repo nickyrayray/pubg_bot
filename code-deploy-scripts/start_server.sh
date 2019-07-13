e48cfff126494fa125f4db9de65ee82af1e3bf90
nickyrayray <Enterprise.1701>
2019-07-13 13:58:33 -0400

    Use supervisor

--- code-deploy-scripts/start_server.sh
+++ code-deploy-scripts/start_server.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-cd /app/pubg_bot
-
-. venv/bin/activate
-
-gunicorn server.pubg_server:app --bind 0.0.0.0:8000 --daemon
+supervisorctl start pubg_bot
---
+++ code-deploy-scripts/stop_server.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+supervisorctl stop pubg_bot
